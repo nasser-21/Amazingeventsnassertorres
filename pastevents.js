4929b17c9b0bbd664da7a9ffd99870ef1a0ebd82
nasser torres h <nasserhth@gmail.com>
2023-10-17 19:50:50 -0500

    nuevo trabajo

---
+++ pastevents.js
@@ -0,0 +1,20 @@
+const contenedor = document.getElementById("contenedorcards")
+const  events=data.events
+for (let cards of events ) {
+  
+const card= document.createElement("div")
+card.classList.add("card")
+card.style.width="15rem"
+card.innerHTML=`
+  <img src="${cards.image}" class="card-img-top imgcards" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${cards.name}</h5>
+    <p class="card-text">${cards.description}</p>
+  </div>
+  <div class="d-flex justify-content-between p-4">
+    <h6> Price$ ${cards.price}</h6>
+    <a class="btn btn-secondary " role="button" href="details.html" aria-disabled="true">Details</a>
+   </div>` 
+contenedor.appendChild(card)
+
+}
